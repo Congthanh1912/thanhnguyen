c884f8c2375a1cbfcb96dc1700edf5f7009f0165
Congthanh1912 <nguyencongthanh19122006@gmail.com>
2025-06-14 20:50:51 +0700

    bai tap buoi 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.ComponentModel.Design;
+namespace haha
+{
+    public static class Thanhnn
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("so tien gui");
+            float tiengoi = float.Parse(Console.ReadLine());
+            Console.WriteLine("phan tram lai suat");
+            float phantramlaisuat = float.Parse(Console.ReadLine());
+            Console.WriteLine("So nam gui");
+            int sonamgui = int.Parse(Console.ReadLine());
+
+            double tiennhan = tiengoi * Math.Pow((1 + phantramlaisuat / 100), sonamgui);
+            Console.WriteLine($"tien nhan{tiennhan}");
+
+
+
+
+
+
+
+
+        }
+    }
+}
